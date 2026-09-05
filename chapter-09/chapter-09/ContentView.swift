9569f7ab3c2abfe9ec4d2e484df910a94ad2bf08
Junhyunny <kang3966@naver.com>
2026-09-05 09:34:49 +0900

    Add stopwatch state example to chapter 09

--- chapter-09/chapter-09/ContentView.swift
+++ chapter-09/chapter-09/ContentView.swift
@@ -5,17 +5,36 @@
 //  Created by 강준현 on 9/3/26.
 //
 
+// TODO: [todos/combine.md](../../todos/combine.md)
+internal import Combine
 import SwiftUI
 
+final class StopWatchViewModel: ObservableObject {
+    @Published var elapsedTime = 0
+    var timer: Timer?
+    func start() {
+        if timer != nil {
+            return
+        }
+        timer = Timer.scheduledTimer(
+            withTimeInterval: 1,
+            repeats: true,
+            block: { [weak self] _ in
+                self?.elapsedTime += 1
+            }
+        )
+    }
+}
+
 struct ContentView: View {
+    @StateObject private var vm = StopWatchViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("elapsed time: \(vm.elapsedTime)")
+            Button("Start") {
+                vm.start()
+            }
         }
-        .padding()
     }
 }
 
